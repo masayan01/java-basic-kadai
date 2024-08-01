cb727aa8ae55a32582ccfd9e82de52fd1c536c65
masayan01 <masamasamasa0806@icloud.com>
2024-08-01 16:45:42 +0900

    Add files via upload

---
+++ kadai_015/kadai_015/src/CarExec_chapter015.java
@@ -0,0 +1,9 @@
+public class CarExec_chapter015 {
+    public static void main(String[] args){
+        Car_chapter15 voxy = new Car_chapter15(1,10);
+        voxy.gearChange(3);
+        voxy.run();
+            
+    }
+    
+}
