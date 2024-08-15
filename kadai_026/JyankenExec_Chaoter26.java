9ffbebc744b11038ca5bff7fb747dc7b640be5a6
masayan01 <masamasamasa0806@icloud.com>
2024-08-15 22:40:17 +0900

    Add files via upload

---
+++ kadai_026/JyankenExec_Chaoter26.java
@@ -0,0 +1,9 @@
+package kadai_026;
+
+public class JyankenExec_Chaoter26 {
+    public static void mian(String[] args){
+            Jyanken_Chapter26 janken = new Jyanken_Chapter26();
+
+            janken.playGame();
+    }
+}
