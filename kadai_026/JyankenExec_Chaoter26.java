6a548f107e0c6cce0432a2cac1e7cde9d560cd7f
masayan01 <masamasamasa0806@icloud.com>
2024-08-15 22:48:56 +0900

    Delete kadai_026 directory

--- kadai_026/JyankenExec_Chaoter26.java
+++
@@ -1,9 +0,0 @@
-package kadai_026;
-
-public class JyankenExec_Chaoter26 {
-    public static void mian(String[] args){
-            Jyanken_Chapter26 janken = new Jyanken_Chapter26();
-
-            janken.playGame();
-    }
-}
